007f8b6c14e013d0fb30c3e759c6d6696b5b568c
Miguel Gimenez <miguel.gimenez@primaverasound.com>
2018-12-31 18:15:01 +0100

    wip

--- soundRecordingsApp/serializers.py
+++ soundRecordingsApp/serializers.py
@@ -1,15 +1,8 @@
 from rest_framework import serializers
-from soundRecordingsApp.models import SoundRecording, SoundRecordingInput
+from soundRecordingsApp.models import SoundRecording, SoundRecordingInput, SimilarityScores
 
 
 class SoundRecordingModelSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = SoundRecording
-		fields =  '__all__'
-
-class SoundRecordingMatchSerializer(serializers.ModelSerializer):
-	score = serializers.IntegerField(read_only=True)
-
 	class Meta:
 		model = SoundRecording
 		fields =  '__all__'
@@ -21,3 +14,39 @@ class SoundRecordingInputModelSerializer(serializers.ModelSerializer):
 		model = SoundRecordingInput
 		fields =  '__all__'
 
+
+
+
+class SoundRecordingMatchSerializer(serializers.ModelSerializer):
+	score = serializers.IntegerField(read_only=True)
+
+	class Meta:
+		model = SoundRecording
+		fields =  '__all__'
+
+
+class SoundRecordingInputMatchesSerializer(serializers.ModelSerializer):
+	matches = serializers.SerializerMethodField()
+
+	class Meta:
+		model = SoundRecordingInput
+		fields =  '__all__'
+	# I am not sure this is the most efficient way.
+	def get_matches(self, obj):
+
+			matches = []
+
+			similarityScores = SimilarityScores.objects.filter(soundRecordingInput_id=obj.id)
+			
+			for match in similarityScores :
+				
+				soundRecording = SoundRecording.objects.get(id=match.soundRecording.id)
+				
+				soundRecording.score = match.score
+				
+				serializer = SoundRecordingMatchSerializer(soundRecording)
+				matches.append(serializer.data)
+
+			return matches
+
+			
