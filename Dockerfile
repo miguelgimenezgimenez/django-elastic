d7035358e8f5fd25d0f3e46f03ea1323acd6d184
Miguel Gimenez <miguel.gimenez@primaverasound.com>
2019-01-01 21:20:19 +0100

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,3 @@ RUN [ -d /api ] || mkdir /api;
 COPY  ./ /api
 WORKDIR /api
 RUN pip install -r  requirements.txt
-RUN  docker-compose run web python manage.py search_index --create
--- search-index-create.sh
+++ search-index-create.sh
@@ -1,2 +1,9 @@
-if /usr/local/lib/python3.6/site-packages/django_elasticsearch_dsl/management/commands/search_index.py
-python manage.py search_index --create
+#!/usr/bin/env bash
+
+if [ ! -f /usr/local/lib/python3.6/site-packages/django_elasticsearch_dsl/management/commands/search_index.py ]; then
+    echo "Creating search index"
+    python manage.py search_index --create
+    
+else
+   echo "Search index allready exist."
+fi
